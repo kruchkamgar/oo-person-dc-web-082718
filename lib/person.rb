ae14b40091bc0ef859661fb9d879af9f1fd4dc33
github username <github email address>
2018-08-29 17:01:49 -0400

    finished internal methods

--- lib/person.rb
+++ lib/person.rb
@@ -22,17 +22,7 @@ class Person
 		else #don't modify input if 10 > happiness > 0
 			@happiness = happiness
 		end
-
-
-		# if (happiness <= 10 && happiness >= 0)
-
-		# 	@happiness = happiness
-		# # else
-		# # 	puts "enter a # b/n 0 and 10"
-		# end
-
 	end
-
 
 	def hygiene=(hygiene)
 		if hygiene > 10
@@ -44,4 +34,65 @@ class Person
 			@hygiene = hygiene
 		end
 	end
+
+	def happy?
+		if @happiness > 7
+			return true
+		else
+			return false
+		end
+	end
+
+	def clean?
+		if @hygiene > 7
+			return true
+		else
+			return false
+		end
+	end
+
+	def get_paid(salary_amount)
+		@bank_account += salary_amount
+		return "all about the benjamins"
+	end
+
+	def take_bath
+		@hygiene += 4
+		self.hygiene=(@hygiene)
+		#what is self. purpose? why can't we call another method within a class directly?
+		return "♪ Rub-a-dub just relaxing in the tub ♫"
+	end
+
+	def work_out
+		@happiness += 2
+		self.happiness=(@happiness)
+		@hygiene -= 3
+		self.hygiene=(@hygiene)
+		return "♪ another one bites the dust ♫"
+	end
+
+	def call_friend(person)
+		@happiness += 3
+		self.happiness=(@happiness)
+		person.happiness +=3
+		return "Hi #{person.name}! It's #{@name}. How are you?"
+	end
+
+	def start_conversation(person, topic)
+		case topic
+		when "politics"
+			@happiness -=2
+			self.happiness=(@happiness)
+			person.happiness -=2
+			return "blah blah partisan blah lobbyist"
+		when "weather"
+			@happiness +=1
+			self.happiness=(@happiness)
+			person.happiness +=1
+			return "blah blah sun blah rain"
+		else
+			return "blah blah blah blah blah"
+		end
+	end
+
 end
